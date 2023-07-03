6dedfa7b7867510a3c8234ab3485639d5fafdcdf
junsu0573 <junsu0573@naver.com>
2023-07-03 18:20:17 +0900

    edit models

--- deep_plant_app/lib/models/meat_data_model.dart
+++ deep_plant_app/lib/models/meat_data_model.dart
@@ -1,30 +1,45 @@
 class MeatData {
+  String? imageFile;
+
+  // 관리 번호 생성 시 firebase firestore 에 저장하는 데이터
+  String? userEmail;
+  String? saveTime;
   String? historyNumber;
   String? species;
   String? lDivision;
   String? sDivision;
-  String? imageFile;
-  String? date;
-  Map<String, double>? freshData;
-
-  String? userEmail;
   String? gradeNm;
   String? farmAddr;
   String? butcheryPlaceNm;
   String? butcheryYmd;
+  Map<String, double>? freshData;
+
+  // 연구 데이터 추가 입력 완료 시 firebase firestore에 저장하는 데이터
+  List<String>? deepAging;
+  Map<String, double>? heatedMeat;
+  Map<String, double>? tongueData;
+  Map<String, double>? labData;
 
   MeatData({
+    this.imageFile,
+
+    // 관리 번호 생성 시 firebase firestore 에 저장하는 데이터
+    this.userEmail,
+    this.saveTime,
     this.historyNumber,
     this.species,
     this.lDivision,
     this.sDivision,
-    this.imageFile,
-    this.date,
-    this.freshData,
-    this.userEmail,
     this.gradeNm,
     this.farmAddr,
     this.butcheryPlaceNm,
     this.butcheryYmd,
+    this.freshData,
+
+    // 연구 데이터 추가 입력 완료 시 firebase firestore에 저장하는 데이터
+    this.deepAging,
+    this.heatedMeat,
+    this.tongueData,
+    this.labData,
   });
 }
