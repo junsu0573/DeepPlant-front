200e09bf22d9fadf814873f927c8c30ce00950d7
junsu0573 <junsu0573@naver.com>
2023-07-07 10:59:12 +0900

    data saving

--- deep_plant_app/lib/pages/maet-registration/additional-data/show_step_2_page.dart
+++ deep_plant_app/lib/pages/maet-registration/additional-data/show_step_2_page.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:deep_plant_app/models/meat_data_model.dart';
 import 'package:deep_plant_app/models/user_model.dart';
 import 'package:deep_plant_app/widgets/custom_appbar.dart';
 import 'package:deep_plant_app/widgets/save_button.dart';
+import 'package:deep_plant_app/widgets/show_custom_dialog.dart';
 import 'package:deep_plant_app/widgets/step_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ShowStep2 extends StatefulWidget {
   final UserModel user;
@@ -23,6 +28,7 @@ class ShowStep2 extends StatefulWidget {
 class _ShowStep2State extends State<ShowStep2> {
   bool _isAllCompleted() {
     if (widget.meat.deepAging != null &&
+        widget.meat.freshData != null &&
         widget.meat.heatedMeat != null &&
         widget.meat.tongueData != null &&
         widget.meat.labData != null) {
@@ -31,10 +37,103 @@ class _ShowStep2State extends State<ShowStep2> {
     return false;
   }
 
+  // 임시 데이터를 로컬 임시 파일로 저장
+  Future<void> saveDataToLocal(Map<String, dynamic> data) async {
+    final directory = await getTemporaryDirectory();
+    final file = File('${directory.path}/temp_data_2.json');
+
+    await file.writeAsString(jsonEncode(data));
+  }
+
+  // 객체 데이터를 임시 저장 데이터로 초기화
+  Future<void> initMeatdata() async {
+    final directory = await getTemporaryDirectory();
+    final file = File('${directory.path}/temp_data.json');
+    if (await file.exists()) {
+      final jsonData = await file.readAsString();
+      final data = jsonDecode(jsonData);
+
+      widget.meat.deepAging = data['deepAging'];
+      if (data['freshData'] != null) {
+        widget.meat.freshData = data['freshData']?.cast<String, double>();
+      } else {
+        widget.meat.freshData = null;
+      }
+      if (data['heatedMeat'] != null) {
+        widget.meat.heatedMeat = data['heatedMeat']?.cast<String, double>();
+      } else {
+        widget.meat.heatedMeat = null;
+      }
+      if (data['tongueData'] != null) {
+        widget.meat.tongueData = data['tongueData']?.cast<String, double>();
+      } else {
+        widget.meat.tongueData = null;
+      }
+      if (data['labData'] != null) {
+        widget.meat.labData = data['labData']?.cast<String, double>();
+      } else {
+        widget.meat.labData = null;
+      }
+    }
+  }
+
+  // 임시저장 데이터 저장
+  Future<void> saveTempData() async {
+    // 데이터 생성
+    Map<String, dynamic> tempBasicData = {
+      'deepAging': widget.meat.historyNumber,
+      'heatedMeat': widget.meat.species,
+      'tongueData': widget.meat.lDivision,
+      'labData': widget.meat.sDivision,
+    };
+    await saveDataToLocal(tempBasicData);
+  }
+
+  // 임시저장 데이터 리셋
+  Future<void> resetTempData() async {
+    // 데이터 생성
+    Map<String, dynamic> tempBasicData = {
+      'deepAging': null,
+      'heatedMeat': null,
+      'tongueData': null,
+      'labData': null,
+    };
+
+    await saveDataToLocal(tempBasicData);
+  }
+
+  void initialize() async {
+    // 임시저장 데이터를 가져와 객체에 저장
+    await initMeatdata().then((_) {
+      setState(() {});
+    });
+
+    if (widget.meat.deepAging != null ||
+        widget.meat.freshData != null ||
+        widget.meat.heatedMeat != null ||
+        widget.meat.tongueData != null && widget.meat.labData != null) {
+      if (!mounted) return;
+      // 임시저장 데이터가 null값이 아닐 때 다이얼로그 호출
+      showDataRegisterDialog(context, () async {
+        // 처음부터
+        resetTempData();
+        await initMeatdata().then((_) {
+          setState(() {});
+        });
+        if (!mounted) return;
+        context.pop();
+      }, () {
+        // 이어서
+        context.pop();
+      });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     setState(() {});
+    initialize();
   }
 
   @override
@@ -87,7 +186,7 @@ class _ShowStep2State extends State<ShowStep2> {
               ),
             ),
             GestureDetector(
-              onTap: () => context.go('/option/show-step-2/hitted-meat-data'),
+              onTap: () => context.go('/option/show-step-2/tongue-data'),
               child: StepCard(
                 mainText: '전자혀 데이터',
                 subText: '데이터를 입력해 주세요.',
@@ -96,7 +195,7 @@ class _ShowStep2State extends State<ShowStep2> {
               ),
             ),
             GestureDetector(
-              onTap: () => context.go('/option/show-step-2/tongue-data'),
+              onTap: () => context.go('/option/show-step-2/experiment-data'),
               child: StepCard(
                 mainText: '실험 데이터',
                 subText: '데이터를 입력해 주세요.',
