4ac4488160b22d8dfcaadfc140f1449b6f387b94
junsu0573 <junsu0573@naver.com>
2023-07-10 10:55:45 +0900

    validation pwchange

--- deep_plant_app/lib/pages/my-page/edit_user_info_page.dart
+++ deep_plant_app/lib/pages/my-page/edit_user_info_page.dart
@@ -24,225 +24,229 @@ class EditUserInfo extends StatelessWidget {
           FocusScope.of(context).unfocus();
         },
         child: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 62.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '아이디',
-                  style: TextStyle(
-                    fontSize: 30.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(
-                  height: 10.h,
-                ),
-                Container(
-                  height: 85.h,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(232, 232, 232, 1),
-                    borderRadius: BorderRadius.circular(42.sp),
-                  ),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 32.w,
-                      ),
-                      Text(
-                        '${user.email}',
-                        style: TextStyle(
-                          fontSize: 30.sp,
-                          fontWeight: FontWeight.w400,
-                          color: Color.fromRGBO(153, 153, 153, 1),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 36.h,
-                ),
-                Text(
-                  '비밀번호',
-                  style: TextStyle(
-                    fontSize: 30.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(
-                  height: 10.h,
-                ),
-                // 비밀번호 변경 버튼
-                CommonButton(
-                  text: Text('비밀번호 변경하기'),
-                  onPress: () {
-                    context.go('/option/my-page/edit-info/reset-pw');
-                  },
-                  width: 500,
-                  height: 85.h,
-                ),
-                SizedBox(
-                  height: 36.h,
-                ),
-                Text(
-                  '주소',
-                  style: TextStyle(
-                    fontSize: 30.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(
-                  height: 27.h,
-                ),
-                Row(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 62.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(
-                      width: 412.w,
-                      height: 50.h,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: '주소',
-                        ),
-                        keyboardType: TextInputType.number,
+                    Text(
+                      '아이디',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                    Spacer(),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Container(
+                      height: 85.h,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(232, 232, 232, 1),
+                        borderRadius: BorderRadius.circular(42.sp),
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 32.w,
+                          ),
+                          Text(
+                            '${user.email}',
+                            style: TextStyle(
+                              fontSize: 30.sp,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromRGBO(153, 153, 153, 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 36.h,
+                    ),
+                    Text(
+                      '비밀번호',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    // 비밀번호 변경 버튼
                     CommonButton(
-                      text: Text(
-                        '검색',
-                        style: TextStyle(
-                          fontSize: 30.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      onPress: () {},
-                      width: 125.w,
-                      height: 75.h,
-                      bgColor: Color.fromRGBO(46, 48, 62, 1),
+                      text: Text('비밀번호 변경하기'),
+                      onPress: () {
+                        context.go('/option/my-page/edit-info/reset-pw');
+                      },
+                      width: 500,
+                      height: 85.h,
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 34.h,
-                ),
-                SizedBox(
-                  height: 50.h,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: '상세주소 (동/호수)',
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                SizedBox(
-                  height: 68.h,
-                ),
-                Text(
-                  '회사정보',
-                  style: TextStyle(
-                    fontSize: 30.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(
-                  height: 27.h,
-                ),
-                Row(
-                  children: [
                     SizedBox(
-                      width: 412.w,
+                      height: 36.h,
+                    ),
+                    Text(
+                      '주소',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 27.h,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 412.w,
+                          height: 50.h,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: '주소',
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Spacer(),
+                        CommonButton(
+                          text: Text(
+                            '검색',
+                            style: TextStyle(
+                              fontSize: 30.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          onPress: () {},
+                          width: 125.w,
+                          height: 75.h,
+                          bgColor: Color.fromRGBO(46, 48, 62, 1),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 34.h,
+                    ),
+                    SizedBox(
                       height: 50.h,
                       child: TextField(
                         decoration: InputDecoration(
-                          hintText: '주소',
+                          hintText: '상세주소 (동/호수)',
                         ),
                         keyboardType: TextInputType.number,
                       ),
                     ),
-                    Spacer(),
-                    CommonButton(
-                      text: Text(
-                        '검색',
-                        style: TextStyle(
-                          fontSize: 30.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      onPress: () {},
-                      width: 125.w,
-                      height: 75.h,
-                      bgColor: Color.fromRGBO(46, 48, 62, 1),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 34.h,
-                ),
-                SizedBox(
-                  height: 50.h,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: '회사명',
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                SizedBox(
-                  height: 34.h,
-                ),
-                Row(
-                  children: [
                     SizedBox(
-                      width: 235.w,
+                      height: 68.h,
+                    ),
+                    Text(
+                      '회사정보',
+                      style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 27.h,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 412.w,
+                          height: 50.h,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: '주소',
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Spacer(),
+                        CommonButton(
+                          text: Text(
+                            '검색',
+                            style: TextStyle(
+                              fontSize: 30.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          onPress: () {},
+                          width: 125.w,
+                          height: 75.h,
+                          bgColor: Color.fromRGBO(46, 48, 62, 1),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 34.h,
+                    ),
+                    SizedBox(
                       height: 50.h,
                       child: TextField(
                         decoration: InputDecoration(
-                          hintText: '부서명',
+                          hintText: '회사명',
                         ),
                         keyboardType: TextInputType.number,
                       ),
                     ),
-                    Spacer(),
                     SizedBox(
-                      width: 235.w,
+                      height: 34.h,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 235.w,
+                          height: 50.h,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: '부서명',
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Spacer(),
+                        SizedBox(
+                          width: 235.w,
+                          height: 50.h,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: '직위',
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 34.h,
+                    ),
+                    SizedBox(
                       height: 50.h,
                       child: TextField(
                         decoration: InputDecoration(
-                          hintText: '직위',
+                          hintText: '전화번호',
                         ),
                         keyboardType: TextInputType.number,
                       ),
                     ),
+                    SizedBox(
+                      height: 81.h,
+                    ),
                   ],
                 ),
-                SizedBox(
-                  height: 34.h,
-                ),
-                SizedBox(
-                  height: 50.h,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: '전화번호',
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                SizedBox(
-                  height: 81.h,
-                ),
-                SaveButton(
-                  text: '저장',
-                  width: 658.w,
-                  heigh: 106.h,
-                  isWhite: false,
-                ),
-                SizedBox(
-                  height: 18.h,
-                ),
-              ],
-            ),
+              ),
+              SaveButton(
+                text: '저장',
+                width: 658.w,
+                heigh: 106.h,
+                isWhite: false,
+              ),
+              SizedBox(
+                height: 45.h,
+              ),
+            ],
           ),
         ),
       ),
--- deep_plant_app/lib/pages/my-page/reset_pw_page.dart
+++ deep_plant_app/lib/pages/my-page/reset_pw_page.dart
@@ -1,9 +1,135 @@
-import 'package:deep_plant_app/widgets/common_button.dart';
+import 'package:deep_plant_app/widgets/save_button.dart';
 import 'package:deep_plant_app/widgets/text_insertion_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ResetPW extends StatelessWidget {
+class ResetPW extends StatefulWidget {
   const ResetPW({super.key});
+
+  @override
+  State<ResetPW> createState() => _ResetPWState();
+}
+
+class _ResetPWState extends State<ResetPW> {
+  // form 구성
+  final _formKey = GlobalKey<FormState>();
+
+  // validation 문구를 위한 변수
+  bool isRedTextPw = false;
+  bool isRedTextCPw = false;
+
+  String userPassword = '';
+  String userNewPassword = '';
+  String userCPassword = '';
+
+  // 버튼 활성화 확인을 위한 변수
+  bool _isValidPw = false;
+  bool _isValidNewPw = false;
+  bool _isValidCPw = false;
+
+  // 기존 비밀번호 유효성 검사
+  String? pwValidate(String? value) {
+    if (value!.isEmpty) {
+      _isValidPw = false;
+      return null;
+    } else if (value != '12341234') {
+      _isValidPw = false;
+      return '비밀번호를 확인하세요.';
+    } else {
+      _isValidPw = true;
+      return null;
+    }
+  }
+
+  // 새 비밀번호 유효성 검사
+  String? newPwValidate(String? value) {
+    final bool isValid = validatePassword(value!);
+    if (value.isEmpty) {
+      setState(() {
+        _isValidNewPw = false;
+        isRedTextPw = false;
+      });
+      return null;
+    } else if (!isValid) {
+      setState(() {
+        _isValidNewPw = false;
+        isRedTextPw = true;
+      });
+      return null;
+    }
+
+    setState(() {
+      _isValidNewPw = true;
+      isRedTextPw = false;
+    });
+
+    return null;
+  }
+
+  // 비밀번호 재입력 유효성 검사
+  String? cPwValidate(String? value) {
+    if (value!.isEmpty) {
+      setState(() {
+        _isValidCPw = false;
+        isRedTextCPw = false;
+      });
+      return null;
+    } else if (userNewPassword != userCPassword) {
+      setState(() {
+        _isValidCPw = false;
+        isRedTextCPw = true;
+      });
+      return null;
+    }
+
+    setState(() {
+      _isValidCPw = true;
+      isRedTextCPw = false;
+    });
+
+    return null;
+  }
+
+  bool validatePassword(String password) {
+    // 비밀번호 유효성을 검사하는 정규식
+    const pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*()\-_=+{};:,<.>]).{10,}$';
+    final regex = RegExp(pattern);
+
+    return regex.hasMatch(password);
+  }
+
+  // 모든 값이 올바르게 입력됐는지 확인
+  bool _isAllValid() {
+    if (_isValidPw && _isValidNewPw && _isValidCPw) {
+      return true;
+    }
+    return false;
+  }
+
+  // 유효성 검사 함수
+  void _tryValidation() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
+  // 비밀번호 변경
+  void changePassword(String newPassword) async {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      try {
+        await user.updatePassword(newPassword);
+      } catch (e) {
+        print('error');
+      }
+    } else {
+      print('user does not exist.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,55 +153,130 @@ class ResetPW extends StatelessWidget {
         },
         child: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 50.0),
-                child: Text('기존 비밀번호'),
-              ),
-              TextInsertionField(
-                validateFunc: null,
-                onSaveFunc: null,
-                onChangeFunc: null,
-                mainText: '',
-                hintText: '',
-                width: 40,
-                isObscure: true,
-                isCenter: true,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 50.0),
-                child: Text('변경할 비밀번호'),
-              ),
-              TextInsertionField(
-                validateFunc: null,
-                onSaveFunc: null,
-                onChangeFunc: null,
-                mainText: '영문+숫자',
-                hintText: '',
-                width: 40,
-                isObscure: false,
-                isCenter: false,
-              ),
-              TextInsertionField(
-                validateFunc: null,
-                onSaveFunc: null,
-                onChangeFunc: null,
-                mainText: '비밀번호 확인',
-                hintText: '',
-                width: 40,
-                isObscure: false,
-                isCenter: false,
+              Form(
+                key: _formKey,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 62.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '기존 비밀번호',
+                        style: TextStyle(
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      TextInsertionField(
+                        validateFunc: (value) => pwValidate(value),
+                        onSaveFunc: null,
+                        onChangeFunc: (value) {
+                          setState(() {
+                            userPassword = value!;
+                            _tryValidation();
+                          });
+                        },
+                        mainText: '',
+                        hintText: '',
+                        width: 0,
+                        isObscure: true,
+                        isCenter: true,
+                      ),
+                      SizedBox(
+                        height: 36.h,
+                      ),
+                      Text(
+                        '변경할 비밀번호',
+                        style: TextStyle(
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      TextInsertionField(
+                        validateFunc: (value) => newPwValidate(value),
+                        onSaveFunc: null,
+                        onChangeFunc: (value) {
+                          setState(() {
+                            userNewPassword = value!;
+                            _tryValidation();
+                          });
+                        },
+                        mainText: '영문 대/소문자+숫자+특수문자',
+                        hintText: '',
+                        width: 0,
+                        isObscure: true,
+                        isCenter: false,
+                      ),
+                      SizedBox(
+                        height: 4.h,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 32.w),
+                        child: Text(
+                          '영문 대/소문자, 숫자, 특수문자로 10자리 이상 구성해주세요.',
+                          style: TextStyle(
+                            color: isRedTextPw
+                                ? Colors.red
+                                : Color.fromRGBO(183, 183, 183, 1),
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.h,
+                      ),
+                      TextInsertionField(
+                        validateFunc: (value) => cPwValidate(value),
+                        onSaveFunc: null,
+                        onChangeFunc: (value) {
+                          userCPassword = value!;
+                          _tryValidation();
+                        },
+                        mainText: '비밀번호 확인',
+                        hintText: '',
+                        width: 0,
+                        isObscure: true,
+                        isCenter: false,
+                      ),
+                      SizedBox(
+                        height: 4.h,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 32.w),
+                        child: Text(
+                          '비밀번호가 일치하지 않습니다.',
+                          style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w400,
+                            color: isRedTextCPw ? Colors.red : Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
               SizedBox(
-                height: 350,
+                height: 500.h,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CommonButton(
-                      text: Text('저장'), onPress: () {}, width: 350, height: 45),
-                ],
+              SaveButton(
+                onPressed: _isAllValid()
+                    ? () {
+                        changePassword(userNewPassword);
+                      }
+                    : null,
+                text: '저장',
+                width: 658.w,
+                heigh: 89.h,
+                isWhite: false,
               ),
             ],
           ),
