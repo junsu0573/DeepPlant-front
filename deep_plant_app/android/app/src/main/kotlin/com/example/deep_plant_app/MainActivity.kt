d06401b4620443d7aa45a4dd9101aabfda2f6e42
junsu0573 <junsu0573@naver.com>
2023-07-11 09:31:22 +0900

    fix barcode operation

--- deep_plant_app/android/app/src/main/kotlin/com/example/deep_plant_app/MainActivity.kt
+++ deep_plant_app/android/app/src/main/kotlin/com/example/deep_plant_app/MainActivity.kt
@@ -1,26 +1,26 @@
-package com.example.deep_plant_app
-
-import io.flutter.embedding.android.FlutterActivity
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "barcode_intent_channel"
+    private var methodChannel: MethodChannel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        handleBarcodeIntent(intent) // 앱이 이미 실행 중일 때 인텐트를 처리하기 위해 추가합니다.
+        // 앱이 이미 실행 중일 때 인텐트를 처리하기 위해 추가
+        handleBarcodeIntent(intent) 
     }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
+        methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+        methodChannel?.setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
             if (call.method == "handleBarcodeIntent") {
                 handleBarcodeIntent(intent)
             }
@@ -29,12 +29,11 @@ class MainActivity : FlutterActivity() {
 
     private fun handleBarcodeIntent(intent: Intent) {
         if (intent.action == "app.dsic.barcodetray.BARCODE_BR_DECODING_DATA") {
-            // 바코드 인식 결과를 처리하는 코드를 작성합니다.
+            // 바코드 인식 결과를 처리하는 코드를 작성
             val symbologyIdent = intent.getStringExtra("EXTRA_BARCODE_DECODED_SYMBOLE")
             val decodedData = intent.getStringExtra("EXTRA_BARCODE_DECODED_DATA")
-            // 결과 데이터를 Flutter로 전달합니다.
-            MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).invokeMethod("onBarcodeDecoded", decodedData)
+            // 결과 데이터를 Flutter로 전달
+            methodChannel?.invokeMethod("onBarcodeDecoded", decodedData)
         }
     }
 }
-
