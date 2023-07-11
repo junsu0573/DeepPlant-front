384233d9acbea7fa27077bac874c9790ccf114ad
junsu0573 <junsu0573@naver.com>
2023-07-11 10:00:27 +0900

    fix

--- deep_plant_app/android/app/src/main/kotlin/com/example/deep_plant_app/MainActivity.kt
+++ deep_plant_app/android/app/src/main/kotlin/com/example/deep_plant_app/MainActivity.kt
@@ -1,20 +1,21 @@
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 
-public class BarcodeBroadcastReceiver extends BroadcastReceiver {
-    private static final String BARCODE_ACTION = "app.dsic.barcodetray.BARCODE_BR_DECODING_DATA";
+class BarcodeBroadcastReceiver : BroadcastReceiver() {
+    companion object {
+        private const val BARCODE_ACTION = "app.dsic.barcodetray.BARCODE_BR_DECODING_DATA"
+    }
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(BARCODE_ACTION)) {
-            String symbologyIdent = intent.getStringExtra("EXTRA_BARCODE_DECODED_SYMBOLE");
-            String decodedData = intent.getStringExtra("EXTRA_BARCODE_DECODED_DATA");
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == BARCODE_ACTION) {
+            val symbologyIdent = intent.getStringExtra("EXTRA_BARCODE_DECODED_SYMBOLE")
+            val decodedData = intent.getStringExtra("EXTRA_BARCODE_DECODED_DATA")
 
             // 바코드 데이터를 Flutter로 전달하는 코드를 작성합니다.
-            Intent flutterIntent = new Intent("barcode_intent_channel");
-            flutterIntent.putExtra("barcodeData", decodedData);
-            context.sendBroadcast(flutterIntent);
+            val flutterIntent = Intent("barcode_intent_channel")
+            flutterIntent.putExtra("barcodeData", decodedData)
+            context?.sendBroadcast(flutterIntent)
         }
     }
 }
